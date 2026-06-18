5c552173f94a0503a744cf565b947e69ff8ec8a2
Erleen <erlenerare@outlook.com>
2026-06-18 21:28:36 +0530

    Palindrome number solution in C++
    Implement a solution to check if a number is a palindrome by reversing it and comparing with the original.

---
+++ leetcode-solved/palindrome-number.cpp
@@ -0,0 +1,33 @@
+/*
+Problem: Palindrome Number
+Approach: Reverse the number and compare with original
+TC: O(log n)
+SC: O(1)
+Difficulty: Easy
+*/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long rev = 0; // to handle overflows
+        int original = x;
+
+        while(x>0){
+            int rem = x%10;
+            rev = rev*10 + rem; 
+            x/=10;
+        }
+
+        // for negative numbers
+        if(original < 0){
+            return false;
+        }
+
+        // for positive numbers
+        if(original == rev){
+            return true;
+        } else {
+            return false;
+        } 
+    }
+};
